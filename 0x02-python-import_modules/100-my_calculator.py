e356da7b14f5fa09aedc2099bd68f5fe27f0d8bc
root <tichi52@gmail.com>
2023-08-15 09:34:04 -0700

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,37 +1,15 @@
-#!/usr/bin/python3
-def add(a, b):
-    """My addition function
-    Args:
-    a: first integer
-    b: second integer
-    Returns:
-    The return value. a + b
-    """
-    return (a + b)
-def sub(a, b):
-    """"My subtraction function
-    Args:
-    a: the first integer
-    b: the second integer
-    Returns:
-    The return value. a - b
-    """
-    return (a - b)
-def mul(a, b):
-    """My multiplication function
-    Args:
-    a: first integer
-    b: second integer
-    Returns:
-    The return value. a*b
-    """
-    return (a * b)
-def div(a, b):
-    """My division function
-    Args:
-    a: first integer
-    b: second integer
-    Returns:
-    The return value. a / b
-    """
-    return (a / b)
+#!/usr/bbin/python3
+
+if __name__ = "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if len(sys.arg) - 1 != 3
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.key()):
+        print("Unknown operator. Available operators: +,-.* and  /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}" .format(a, sys.argv[2], b, ops[sys.argv[2]](a,b)))
